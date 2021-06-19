15e48fe7d7719917dbba969162da9c9e12f2d652
narodnik <x@x.org>
2021-06-19 18:14:36 +0200

    order workouts for gain.py

--- gain.py
+++ gain.py
@@ -11,13 +11,24 @@ def main(argv):
     wdir = argv[1]
     exercise = argv[2]
 
+    workouts = []
+
     for filename in os.listdir(wdir):
         print(f"{filename}")
+
+        date = filename[len("workout_"):][:4]
+        day, month = int(date[:2]), int(date[2:])
+        index = month * 100 + day
+
         with open(f"{wdir}/{filename}") as fd:
             info = json.load(fd)
         for section in info:
             if section["exercise"] == exercise:
-                pprint.pprint(section)
+                workouts.append((index, date, section))
+                #pprint.pprint(section)
+
+    workouts.sort(key=lambda w: w[0])
+    pprint.pprint(workouts)
 
     return 0
 
