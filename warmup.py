c0fb636c86bb10192fa8eec698b99404448c2d27
narodnik <s>
2022-03-18 16:25:17 +0100

    calculate warmup sets weight

---
+++ warmup.py
@@ -0,0 +1,31 @@
+import sys
+import tabulate
+
+def print_stats(first, bar):
+    table = []
+    for i, ratio in enumerate([0.5, 0.7, 0.9]):
+        weight = ratio * first
+        pct = ratio * 100
+        table.append((f"{pct:.0f}", weight, (weight - bar) / 2))
+    print(tabulate.tabulate(table, headers=["percent", "total weight", "plates"]))
+
+def main(argv):
+    if len(argv) != 2:
+        print("error: missing FIRST_REP argument", file=sys.stderr)
+        return -1
+
+    first = float(argv[1])
+
+    # Olympic bar = 20 kg
+    print("Olympic bar:")
+    print_stats(first, 20)
+    print()
+
+    print("Ez curl bar:")
+    print_stats(first, 8.6)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
